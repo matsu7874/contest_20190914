ee58ad7bf5f1101048e51335c1b5ebaae7a929ae
matsu7874 <mtsmtkmt@gmail.com>
2019-08-11 01:54:44 +0900

    [06_mneme_game] add validator by matsu7874

---
+++ 06_mneme_game/tests/validator_matsu7874.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include "testlib.h"
+
+using namespace std;
+
+signed main(int argc, char* argv[]) {
+  registerValidation(argc, argv);
+  long long n = inf.readInt(1, 100000, "N");
+  inf.readEoln();
+
+  string s = inf.readWord("[A-Z]{" + to_string(n) + "}");
+  inf.readEoln();
+
+  int q = inf.readInt(1, 100000, "Q");
+  inf.readEoln();
+
+  long long maxq = (n * (n + 1)) / 2;
+  vector<long long> qs = inf.readLongs(q, 1, maxq, "qs");
+  // inf.readEoln();
+  inf.readEof();
+
+  return 0;
+}
