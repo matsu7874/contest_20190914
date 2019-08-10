8e82b1d9d37d69371c4ddab65345568e01c04bfe
matsu7874 <mtsmtkmt@gmail.com>
2019-08-10 23:25:38 +0900

    [01_hondas_janken] matsu7874解とvalidatorを追加

---
+++ 01_hondas_janken/matsu7874_py_AC/main.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+S = input()
+print('draw' if S[0] == S[1] else 'honda')
---
+++ 01_hondas_janken/tests/validator_matsu7874.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include "testlib.h"
+
+using namespace std;
+
+signed main(int argc, char* argv[]) {
+  registerValidation(argc, argv);
+  inf.readWord("[A-Z]{2}");
+  //   inf.readEoln();
+  inf.readEof();
+  return 0;
+}
